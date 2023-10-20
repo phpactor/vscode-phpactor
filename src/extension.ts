5bc46b6a230a7441f074c47b3e2aa55b1911c32f
Damjan Cvetko <damjan.cvetko@monotek.net>
2023-10-20 18:52:40 +0200

    Use packaged phpactor in vendor and remove update option.

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,7 @@
 import { LanguageClient, ServerOptions, LanguageClientOptions, StreamInfo } from 'vscode-languageclient'
 
 import * as vscode from 'vscode'
-import { spawn } from 'child_process'
-import { existsSync, mkdirSync } from 'fs'
-import { dirname } from 'path'
+import { join } from 'path'
 import * as net from 'net'
 
 const LanguageID = 'php'
@@ -22,13 +20,13 @@ interface PhpactorConfig {
     launchServerArgs: string[]
 }
 
-export async function activate(context: vscode.ExtensionContext): Promise<void> {
+export function activate(context: vscode.ExtensionContext): void {
     const workspaceConfig = vscode.workspace.getConfiguration()
     const config = workspaceConfig.get<PhpactorConfig>('phpactor') || <PhpactorConfig>{}
     const enable = config.enable
 
     if (!config.path) {
-        config.path = await installPhpactor(context.globalStoragePath)
+        config.path = context.asAbsolutePath(join('vendor', 'phpactor', 'phpactor', 'bin', 'phpactor'))
     }
 
     if (enable === false) return
@@ -48,7 +46,7 @@ function getServerOptions(config: PhpactorConfig): ServerOptions {
     let serverOptions
     if (!config.remote.enabled) {
         // launch language server via stdio
-        serverOptions = {
+        serverOptions = <ServerOptions>{
             run: {
                 command: config.path,
                 args: ['language-server', ...config.launchServerArgs],
@@ -98,8 +96,6 @@ function createClient(config: PhpactorConfig): LanguageClient {
     vscode.commands.registerCommand('phpactor.config.dump', dumpConfig)
     vscode.commands.registerCommand('phpactor.services.list', servicesList)
     vscode.commands.registerCommand('phpactor.status', status)
-    const updateConfig = { cwd: dirname(dirname(config.path)) }
-    vscode.commands.registerCommand('phpactor.update', updatePhpactor, updateConfig)
 
     return languageClient
 }
@@ -140,56 +136,4 @@ async function status(): Promise<void> {
     const result = await languageClient.sendRequest<string>('phpactor/status')
     channel.append(result)
     channel.show()
-}
-
-async function installPhpactor(storagePath: string): Promise<string> {
-    if (!existsSync(storagePath)) {
-        mkdirSync(storagePath)
-    }
-
-    const path = `${storagePath}/phpactor`
-
-    if (!existsSync(path)) {
-        const channel = vscode.window.createOutputChannel('Phpactor Installation')
-        void vscode.window.showInformationMessage('Installing Phpactor')
-        await exec(channel, 'git', ['clone', 'https://github.com/phpactor/phpactor', '--depth=1'], storagePath)
-        await exec(channel, 'composer', ['install', '--no-dev'], path)
-        void vscode.window.showInformationMessage(`Phpactor installed at ${path}`)
-    }
-
-    return `${storagePath}/phpactor/bin/phpactor`
-}
-
-export async function updatePhpactor(this: { cwd: string }): Promise<void> {
-    const channel = vscode.window.createOutputChannel('Phpactor Update')
-    channel.appendLine(this.cwd)
-    await exec(channel, 'git', ['pull'], this.cwd)
-    await exec(channel, 'composer', ['install', '--no-dev'], this.cwd)
-    channel.appendLine('Phpactor updated')
-    void vscode.window.showInformationMessage('Phpactor updated')
-}
-
-function exec(channel: vscode.OutputChannel, command: string, args: string[], cwd: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const child = spawn(command, args, {
-            cwd,
-            timeout: 30000,
-        })
-        child.stdout.on('data', (data: Buffer) => {
-            channel.append(data.toString('utf8'))
-        })
-        child.stderr.on('data', (data: Buffer) => {
-            channel.append(data.toString('utf8'))
-        })
-        child.on('close', code => {
-            if (code !== 0) {
-                reject(`Expected git to exit with code 0 got "${code}"`)
-            }
-            resolve()
-        })
-
-        child.on('error', err => {
-            reject(err)
-        })
-    })
 }
